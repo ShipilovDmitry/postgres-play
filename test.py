9e2fab2e5259373efaef1943089a851909a60ffa
Dmitrii <shipilov.dmitry.valer@gmail.com>
2024-05-05 20:30:37 +0300

    better

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
-import psycopg2
+import psycopg
 
-CONNECTION_URI = 'postgresql://habrpguser:pgpwd4habr@127.0.0.1:5432/habrdb'
+CONNECTION_URI = "postgresql://habrpguser:pgpwd4habr@127.0.0.1:5432/habrdb"
+
 try:
-    with psycopg2.connect(CONNECTION_URI) as conn:
+    with psycopg.connect(CONNECTION_URI) as conn:
         with conn.cursor() as cursor:
             cursor = conn.cursor()
-            cursor.execute('SELECT * FROM courses')
+            cursor.execute("SELECT * FROM courses")
             print(cursor.fetchall())
 except:
-    print('can`t connect to database')
+    print("can`t connect to database")
